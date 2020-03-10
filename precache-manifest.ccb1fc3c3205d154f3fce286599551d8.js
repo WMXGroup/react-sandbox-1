e095cfcfcf315b3bbb54b962f2fcb2c158c4b772
Adam DePietropaolo <WMXGroup@gmail.com>
2020-03-09 21:15:42 -0400

    Updates

--- precache-manifest.ccb1fc3c3205d154f3fce286599551d8.js
+++ precache-manifest.ccb1fc3c3205d154f3fce286599551d8.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-sandbox-1/static/js/runtime~main.8fdbdb9b.js"
   },
   {
-    "revision": "94a8cdf4b9c42da1b98b",
-    "url": "/react-sandbox-1/static/js/main.94a8cdf4.chunk.js"
+    "revision": "2e6f30383be80af42f18",
+    "url": "/react-sandbox-1/static/js/main.2e6f3038.chunk.js"
   },
   {
     "revision": "dfd6d9540e1e7e4ff25e",
     "url": "/react-sandbox-1/static/js/1.dfd6d954.chunk.js"
   },
   {
-    "revision": "d66c4fa43e60bda266ddf50744eed2cc",
+    "revision": "38cd3e8c752c9e4277205c5d22726eff",
     "url": "/react-sandbox-1/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-sandbox-1/precache-manifest.62af993502b39022c3bafbe302c3be19.js"
+  "/react-sandbox-1/precache-manifest.ccb1fc3c3205d154f3fce286599551d8.js"
 );
 
 workbox.clientsClaim();
