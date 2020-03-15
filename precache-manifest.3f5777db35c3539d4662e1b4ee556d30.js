e95a463e3c1c8ac8d137a7d26fcfc2f60894f043
Adam DePietropaolo <WMXGroup@gmail.com>
2020-03-15 15:02:36 -0400

    Updates

---
+++ precache-manifest.3f5777db35c3539d4662e1b4ee556d30.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fdbdb9bb99a1ca0350d",
+    "url": "/react-sandbox-1/static/js/runtime~main.8fdbdb9b.js"
+  },
+  {
+    "revision": "0fa01a73f9012303083d",
+    "url": "/react-sandbox-1/static/js/main.0fa01a73.chunk.js"
+  },
+  {
+    "revision": "6b6efeaf546691f28c44",
+    "url": "/react-sandbox-1/static/js/1.6b6efeaf.chunk.js"
+  },
+  {
+    "revision": "855ae574db8965ac211ad3c8682f644e",
+    "url": "/react-sandbox-1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-sandbox-1/precache-manifest.d7284cb172360923c9582b7542e36041.js"
+  "/react-sandbox-1/precache-manifest.3f5777db35c3539d4662e1b4ee556d30.js"
 );
 
 workbox.clientsClaim();
